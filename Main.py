34028626a74f9b61df74a649dac9d57e71a64214
Damien MARTINS GOMES <92399107+Damien3008@users.noreply.github.com>
2022-11-25 15:29:32 -0500

    Delete Main.py

--- Main.py
+++
@@ -1,7 +0,0 @@
-from MP2 import Rush_Hour_Search
-from tqdm import tqdm
-
-for game in tqdm(["Game 1", "Game 2", "Game 3", "Game 4", "Game 5", "Game 6"]):
-    r = Rush_Hour_Search(game)
-    r.gbfs()
-
