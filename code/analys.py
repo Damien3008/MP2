b50365ea228cf6bc19fae818cb0f2d66eafe3098
Damien MARTINS GOMES <92399107+Damien3008@users.noreply.github.com>
2022-11-30 23:49:48 -0500

    add comments

--- code/analys.py
+++ code/analys.py
@@ -1,9 +1,26 @@
+# =============================================================================
+# Mini Project 2: Rush Hours.
+# Analysis code.
+# =============================================================================
+
+from MP2 import Rush_Hour_Search
+from tqdm import tqdm
+
+Games = ["Game " + str(i) for i in range(1,3)]
+heuristic = [(1,1,1), (1,2,1), (1,3,5), (1,4,1), (1,5,1)]
+for game in tqdm(Games):
+    for h in heuristic:
+        r = Rush_Hour_Search(game , input_file = "games_file.txt")
+        r.gbfs(heuristic = h)
+
+
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
-A_star_folder= "/home/damien/Desktop/School/Concordia/COMP_472/Mini_projects/MP2/A_star_tests"
-gbfs_folder = "/home/damien/Desktop/School/Concordia/COMP_472/Mini_projects/MP2/gbfs_tests"
-ucs_folder = "/home/damien/Desktop/School/Concordia/COMP_472/Mini_projects/MP2/ucs_tests"
+
+A_star_folder= "/home/damien/Desktop/SCHOOL/Concordia/COMP_472/Mini_projects/MP2/A_tests"
+gbfs_folder = "/home/damien/Desktop/SCHOOL/Concordia/COMP_472/Mini_projects/MP2/gbfs_tests"
+ucs_folder = "/home/damien/Desktop/SCHOOL/Concordia/COMP_472/Mini_projects/MP2/ucs_tests"
 fichiers_star = [f for f in listdir(A_star_folder) if isfile(join(A_star_folder, f))]
 fichiers_gbfs = [f for f in listdir(gbfs_folder) if isfile(join(gbfs_folder, f))]
 fichiers_ucs = [f for f in listdir(ucs_folder) if isfile(join(ucs_folder, f))]
@@ -15,11 +32,11 @@ buffer = list()
 for files in files_all:
     for file in files:
         if files == files_all[0]:
-            f = open("/home/damien/Desktop/School/Concordia/COMP_472/Mini_projects/MP2/A_star_tests/" + file, 'r')
+            f = open("/home/damien/Desktop/SCHOOL/Concordia/COMP_472/Mini_projects/MP2/A_tests/" + file, 'r')
         elif files == files_all[1]:
-            f = open("/home/damien/Desktop/School/Concordia/COMP_472/Mini_projects/MP2/gbfs_tests/" + file, 'r')
+            f = open("/home/damien/Desktop/SCHOOL/Concordia/COMP_472/Mini_projects/MP2/gbfs_tests/" + file, 'r')
         else:
-            f = open("/home/damien/Desktop/School/Concordia/COMP_472/Mini_projects/MP2/ucs_tests/" + file, 'r')
+            f = open("/home/damien/Desktop/SCHOOL/Concordia/COMP_472/Mini_projects/MP2/ucs_tests/" + file, 'r')
         try:
             content = f.readlines()[10:13]
             if content[0][0] == 'R':
@@ -59,26 +76,28 @@ for i in range(len(content_star)):
     except:
         continue
 
-data = pd.DataFrame(cont, columns=["puzzle number", "algorithm", "heuristic", "length of the solution", "length of the path", "Execution time (seconds)"])
+data = pd.DataFrame(cont, columns=["puzzle number", "algorithm", "heuristic", "length of the search path","length of the solution", "Execution time (seconds)"])
+data.to_csv('result.csv')
 info = list()
 for algo in ['ucs', 'gbfs', 'A*']:
     buf = list()
     if algo == 'ucs':
-        for value in ["length of the solution", "length of the path", "Execution time (seconds)"]:
+        for value in ["length of the search path","length of the solution", "Execution time (seconds)"]:
             v = data.loc[data['algorithm'] == 'ucs', value].sum() / len(data.loc[data['algorithm'] == 'ucs', value])
             buf.append(round(v, 4))
     else:
-        for h in ['h1', 'h2', 'h3', 'h4']:
-            for value in ["length of the solution", "length of the path", "Execution time (seconds)"]:
+        for h in ['h1', 'h2', 'h3', 'h4', 'h5']:
+            for value in ["length of the search path","length of the solution", "Execution time (seconds)"]:
                 v = data.loc[(data['algorithm'] == algo)  & (data['heuristic'] == h), value].sum()  / \
                     len(data.loc[(data['algorithm'] == algo)  & (data['heuristic'] == h), value])
                 buf.append(round(v, 4))
     info.append(buf)
 
-columns = ['length of the solution (for gbfs and A* heuristic = 1)','length of the path (for gbfs and A* heuristic = 1)',
-           'Execution time (seconds) (for gbfs and A* heuristic = 1)', 'length of the solution (for gbfs and A* heuristic = 2)','length of the path (for gbfs and A* heuristic = 2)',
-           'Execution time (seconds) (for gbfs and A* heuristic = 2)', 'length of the solution (for gbfs and A* heuristic = 3)','length of the path (for gbfs and A* heuristic = 3)',
-           'Execution time (seconds) (for gbfs and A* heuristic = 3)', 'length of the solution (for gbfs and A* heuristic = 4)','length of the path (for gbfs and A* heuristic = 4)',
-           'Execution time (seconds) (for gbfs and A* heuristic = 4)']
+columns = ['length of the search path (for gbfs and A* heuristic = 1)','length of the solution (for gbfs and A* heuristic = 1)',
+           'Execution time (seconds) (for gbfs and A* heuristic = 1)', 'length of the search path (for gbfs and A* heuristic = 2)','length of the solution (for gbfs and A* heuristic = 2)',
+           'Execution time (seconds) (for gbfs and A* heuristic = 2)', 'length of the search path (for gbfs and A* heuristic = 3)','length of the solution (for gbfs and A* heuristic = 3)',
+           'Execution time (seconds) (for gbfs and A* heuristic = 3)', 'length of the search path (for gbfs and A* heuristic = 4)','length of the solution (for gbfs and A* heuristic = 4)',
+           'Execution time (seconds) (for gbfs and A* heuristic = 4)', 'length of the search path (for gbfs and A* heuristic = 5)','length of the solution (for gbfs and A* heuristic = 5)',
+           'Execution time (seconds) (for gbfs and A* heuristic = 5)']
 data_mean  =pd.DataFrame(info, index=['ucs', 'gbfs','A*'], columns=columns)
 data_mean.to_csv('analys.csv')
